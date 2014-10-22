ae992501b68928e1ab2ddbd91909e48cdeb29796
Martijn Brekhof <martijn@atcomputing.nl>
2014-10-22 16:09:24 +0200

    * Changed splash animation

--- src/nl/atcomputing/refcard/SplashScreen.java
+++ src/nl/atcomputing/refcard/SplashScreen.java
@@ -3,53 +3,42 @@ package nl.atcomputing.refcard;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class SplashScreen extends Activity {	
-	protected int _splashTime = 2000; 	// in milliseconds
-	private Thread splashTread;
-
-	/** Called when the activity is first created. */
-	@Override
+public class SplashScreen extends Activity implements AnimationListener {	
+	
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.splash);
-	    
-	    final SplashScreen sPlashScreen = this; 
-	    
-	    // thread for displaying the SplashScreen
-	    splashTread = new Thread() {
-	        @Override
-	        public void run() {
-	            try {	            	
-	            	synchronized(this){
-	            		wait(_splashTime);
-	            	}
-	            	
-	            } catch(InterruptedException e) {} 
-	            finally {
-	                finish();
-	                
-	                // activate the screen showing the TABs
-	                Intent i = new Intent();
-	                i.setClass(sPlashScreen, ATComputingrefcardActivity.class);
-	        		startActivity(i);
-	                
-	                //stop();
-	            }
-	        }
-	    };
-	    
-	    splashTread.start();
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.splash);		
+
+		ImageView imageView= (ImageView)findViewById(R.id.logo);
+		Animation animation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+		animation.setAnimationListener(this);
+		imageView.startAnimation(animation);
 	}
 
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-	    	synchronized(splashTread){
-	    		splashTread.notifyAll();
-	    	}
-	    }
-	    return true;
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		Intent intent = new Intent(SplashScreen.this, ATComputingrefcardActivity.class);
+		startActivity(intent);
+		overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+		finish();
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+		
 	}
 }
