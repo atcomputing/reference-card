55d78898302c51ff9adf9ff29afa73d58ffe8553
Martijn Brekhof <m.brekhof@gmail.com>
2015-09-18 11:54:18 +0200

    Added copyright statements and license info

--- splashScreen/src/main/java/nl/atcomputing/refcard/fragments/CommandFragment.java
+++ splashScreen/src/main/java/nl/atcomputing/refcard/fragments/CommandFragment.java
@@ -1,3 +1,24 @@
+/**
+ * 
+ * Copyright 2014 AT Computing BV
+ *
+ * This file is part of Linux Reference Card.
+ *
+ * Linux Reference Card is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Linux Reference Card is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Linux Reference Card.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package nl.atcomputing.refcard.fragments;
 
 import android.os.Bundle;
